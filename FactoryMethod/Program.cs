9e26e00f2df827cda3c766aa3c68be0ba5cd821b
João Paulo Ferreira França <jpdotfranca@outlook.com>
2024-03-22 22:17:30 -0300

    Implementação do FactoryMethod

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace FactoryMethod;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        TransacaoPix transacao = new CriadorPixDinamico();
+        transacao.CriarQrCode(14);
+    }
+}
+
+public abstract class FabricaTransacaoPix : TransacaoPix
+{
+    public abstract TransacaoPix CriarTransacao();
+
+    public string CriarQrCode(int valorPix)
+    {
+       TransacaoPix transacao = CriarTransacao();
+        return transacao.CriarQrCode(valorPix);
+    }
+}
+
+public class CriadorPixDinamico : FabricaTransacaoPix
+{
+    public override TransacaoPix CriarTransacao()
+        => new TransacaoPixDinamico();
+}
+
+public class CriadorPixEstatico : FabricaTransacaoPix
+{
+    public override TransacaoPix CriarTransacao()
+        => new TransacaoPixEstatico();
+}
+
+public interface TransacaoPix
+{
+    string CriarQrCode(int valorPix);
+}
+
+public class TransacaoPixDinamico : TransacaoPix
+{
+    public string CriarQrCode(int valorPix)
+        => "Copia e cola pix dinamico!";
+}
+
+public class TransacaoPixEstatico : TransacaoPix
+{
+    public string CriarQrCode(int valorPix)
+        => "Copia e cola pix estatico!";
+}
